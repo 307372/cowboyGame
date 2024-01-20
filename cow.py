ccf31e0cfc7e27c7a1c251dab2b7e5c62792d362
Kacper Ołubczyński <kolubczynski@pj4323rw5p.lan>
2024-01-20 14:35:38 +0100

    Merge branch 'main' of /Users/kolubczynski/cowboygame/cowboyGame with conflicts.

--- cow.py
+++ cow.py
@@ -1,12 +1,22 @@
 import pygame
 from pos import Pos
 class Cow:
+    # Class attribute to store the loaded texture
+    texture = None
+
     def __init__(self, x, y):
         self.pos = Pos(x, y)
         self.movementSpeed = 2
 
+        # Load the texture only once if it's not already loaded
+        if not Cow.texture:
+            Cow.texture = pygame.image.load("img/cow.gif")  # Replace with the actual path
+
+        # Scale the texture to 200x200 pixels
+        self.texture = pygame.transform.scale(Cow.texture, (200, 200))
+
     def draw(self):
-        pygame.draw.rect(pygame.display.get_surface(), (0,0,0), pygame.Rect(self.pos.x, self.pos.y, 50, 50))
+        pygame.display.get_surface().blit(self.texture, (self.pos.x, self.pos.y))
 
     def moveTowards(self, target):
         # will follow player
--- player.py
+++ player.py
@@ -5,8 +5,23 @@ class Player:
     def __init__(self):
         self.pos = Pos(500, 500)
         self.movementSpeed = 5
+    def __init__(self):
+        self.pos = Pos(500,500)
+        self.width = 50
+        self.height = 100
+        self.texture = pygame.image.load("img/sprite_05.png")
+        if not self.texture:
+            # Load the texture only once if it's not already loaded
+            Player.texture = pygame.image.load("img/sprite_05.png")  # Replace with the actual path
+
+        # Scale the texture to 20x20 pixels
+        self.texture = pygame.transform.scale(self.texture, (200, 200))
+
+
+
     def draw(self):
-        pygame.draw.rect(pygame.display.get_surface(), (255,255,0), pygame.Rect(self.pos.x, self.pos.y, 50, 100))
+        # pygame.draw.rect(pygame.display.get_surface(), (255, 255, 0), pygame.Rect(self.pos.x, self.pos.y, self.width, self.height))
+        pygame.display.get_surface().blit(self.texture, (self.pos.x, self.pos.y))
 
     def move(self):
         keys = pygame.key.get_pressed()
