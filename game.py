83f7f013fa436efa091396b2feb635fdc8ad1955
307372 <307372@uwr.edu.pl>
2024-01-20 14:40:00 +0100

    added cow formation AI

--- game.py
+++ game.py
@@ -1,14 +1,16 @@
 import pygame
 from player import Player
 from cow import Cow
+from pos import Pos
 import random
+import math
 
 class Game:
     screen = 0
 
     def __init__(self, screen):
         self.screen = screen
-        self.cows = [Cow(random.randrange(0, 1000), random.randrange(0, 1000)) for i in range(3)]
+        self.cows = [Cow(random.randrange(0, 1000), random.randrange(0, 1000)) for i in range(60)]
     player = Player()
     def draw(self):
         self.screen.fill((255, 255, 255))
@@ -17,22 +19,25 @@ class Game:
             cow.draw()
 
     def allyAi(self):
-        for cow in self.cows:
-            cow.moveTowards(self.player.pos)
+        for i in range(len(self.cows)):
+            self.cows[i].moveTowards(self.player.pos + self.getCowRingFormationPlayerOffset(len(self.cows), i))
 
     def getCowRingFormationPlayerOffset(self, amountOfCows, cowNumber):
-        radius = 50
+        radius = 200
         deg = 360/amountOfCows
         angle = deg*cowNumber
+        offset = Pos(0,0)
         if angle <= 90:
-            pass
+            offset.x = math.cos(angle) * radius
+            offset.y = math.sin(angle) * radius
         elif angle <= 180:
-            pass
+            offset.x = math.cos(angle - 90) * radius
+            offset.y = -math.sin(angle - 90) * radius
         elif angle <= 270:
             pass
         else:
             pass
-
+        return offset
 
     def run(self):
         self.player.move()
